bc4820b2ac26ca9277e0c2a32235e89767b6bcce
Ron Koppelaar <ron.koppelaar@cegeka.com>
2024-05-15 20:50:44 +0200

    Sign apps from container instead of host (#13)

--- build/Invoke-BuildOnly.ps1
+++ build/Invoke-BuildOnly.ps1
@@ -41,8 +41,7 @@ foreach($AppFolder in $SortedApps) {
 if ($SignApp) {
     New-cdsaAzureDevOpsSection -Message "Signing App $TargetAppFile"
     $CertificateKeyVaultSecret = (Get-AzKeyVaultSecret -VaultName $SecretKeyVaultName -Name $CertificateKeyVaultSecretName).SecretValue
-    Write-Verbose "$CertificateKeyVaultSecret" -Verbose
-    Invoke-cdsaALAppSign -KeyVaultName $CertificateKeyVaultName `
+    Invoke-cdsaALAppSignInContainer -containerName $ContainerName -KeyVaultName $CertificateKeyVaultName `
      -CertificateName $CertificateName `
      -ClientId $ClientId `
      -ClientSecret $CertificateKeyVaultSecret `
