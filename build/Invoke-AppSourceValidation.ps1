19f0fdcb6ceaacc127326c33c9427260a4645a06
Ron Koppelaar <ron.koppelaar@cegeka.com>
2024-05-16 11:55:36 +0200

    Invoke validation build on main or using force parameter

---
+++ build/Invoke-AppSourceValidation.ps1
@@ -0,0 +1,30 @@
+#
+# Trigger Validation Build
+#
+[CmdletBinding()]
+param(
+    #ENV Variables
+    [Parameter(Mandatory = $false)][string] $BuildId = $env:BUILD_BUILDID,
+    [Parameter(Mandatory = $false)][string] $ERPBranchName = $ENV:ERPBranchName,
+    [Parameter(Mandatory = $false)][boolean] $ForceAppSourceValidation = [System.Convert]::ToBoolean($env:FORCEAPPSOURCEVALIDATION),
+    [Parameter(Mandatory = $false)][string] $BuildBranchName = $env:BUILD_SOURCEBRANCHNAME,
+    # Static parameters
+    [Parameter(Mandatory = $false)][string] $AppSourceBuildDefinitionID = 529
+)
+
+$Context = New-cdsaAzureDevOpsAPIContext -Organization 'cegekadsa' `
+    -Project 'DynamicsEmpire' `
+    -PersonalAccessToken $env:TOKEN
+
+if (($BuildBranchName -in ("main","master")) -or $ForceAppSourceValidation) {
+    New-cdsaAzureDevOpsSection -Message "Start 'AppSource Validation' build"
+    Start-cdsaAzureDevOpsBuild -Context $Context `
+        -BuildDefinitionId $AppSourceBuildDefinitionID `
+        -SourceBranch $ERPBranchName `
+        -QueryParameters @{ "api-version" = '5.1' } `
+        -BuildParameters @{ "ERPBuildId" = $BuildId } `
+        -Verbose
+} else {
+    Write-Verbose "No appsource validation - Branchname: $BuildBranchName and ForceAppSourceValidation: $ForceAppSourceValidation"
+}
+New-cdsaAzureDevOpsSection -EndSection
