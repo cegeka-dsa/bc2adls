eb9d5ae5f6a84a9da7a4321205bc504bfbd93f68
Ron Koppelaar <ron.koppelaar@cegeka.com>
2024-05-16 12:44:19 +0200

    Adding parameter definition id

--- build/Invoke-AppSourceValidation.ps1
+++ build/Invoke-AppSourceValidation.ps1
@@ -8,6 +8,7 @@ param(
     [Parameter(Mandatory = $false)][string] $ERPBranchName = $ENV:ERPBranchName,
     [Parameter(Mandatory = $false)][boolean] $ForceAppSourceValidation = [System.Convert]::ToBoolean($env:FORCEAPPSOURCEVALIDATION),
     [Parameter(Mandatory = $false)][string] $BuildBranchName = $env:BUILD_SOURCEBRANCHNAME,
+    [Parameter(Mandatory = $false)][string] $CurrentBuildDefinition = $env:SYSTEM_DEFINITIONID,
     # Static parameters
     [Parameter(Mandatory = $false)][string] $AppSourceBuildDefinitionID = 529
 )
@@ -22,7 +23,7 @@ if (($BuildBranchName -in ("main","master")) -or $ForceAppSourceValidation) {
         -BuildDefinitionId $AppSourceBuildDefinitionID `
         -SourceBranch $ERPBranchName `
         -QueryParameters @{ "api-version" = '5.1' } `
-        -BuildParameters @{ "ERPBuildId" = $BuildId } `
+        -BuildParameters @{ "ERPBuildId" = $BuildId; "ERPALFullBuildDefinitionID" = $CurrentBuildDefinition } `
         -Verbose
 } else {
     Write-Verbose "No appsource validation - Branchname: $BuildBranchName and ForceAppSourceValidation: $ForceAppSourceValidation"
